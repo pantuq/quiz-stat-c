f9104d38af162a0b01f0e8de5be3d4919a3fb73d
tuq <tuq_5986@163.com>
2025-03-11 16:12:50 +0800

    feat: 创建form表单，使用隐藏域存储questionId

--- src/app/question/[id]/page.tsx
+++ src/app/question/[id]/page.tsx
@@ -1,24 +1,31 @@
 import QuestionInput from "@/components/QuestionComponents/QuestionInput";
 import QuestionRadio from "@/components/QuestionComponents/QuestionRadio";
+import styles from "./page.module.scss";
 
 export default async function About({params}: {params: Promise<{id: string}>}) {
     const { id } = await params;
     
     return (
         <div>
-            <h1>Question</h1>
-            <h2>{id}</h2>
             <form>
-                <QuestionInput fe_id="c1" props={{title: '你的姓名',placeholder: "请输入内容"}}/>
-                <QuestionRadio fe_id="c2" props={{
-                    title: '你的性别',
-                    options: [
-                        {value: 'male', text: '男'},
-                        {value: 'female', text: '女'}
-                    ],
-                    value: 'male',
-                    isVertical: false
-                }}/>
+                <input type="hidden" name="questionId" value={id} />
+                <div className={styles.componentWrapper}>
+                    <QuestionInput fe_id="c1" props={{title: '你的姓名',placeholder: "请输入内容"}}/>
+                </div>
+                <div className={styles.componentWrapper}>
+                    <QuestionRadio fe_id="c2" props={{
+                        title: '你的性别',
+                        options: [
+                            {value: 'male', text: '男'},
+                            {value: 'female', text: '女'}
+                        ],
+                        value: 'male',
+                        isVertical: false
+                    }}/>
+                </div>
+                <div className={styles.submitBtnContainer}>
+                    <button type="submit">提交</button>
+                </div>
             </form>
         </div>
     );
