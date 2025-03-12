779d83661b32bb22eaeda819f3380614fc248040
tuq <tuq_5986@163.com>
2025-03-12 17:06:30 +0800

    feat: 根据问卷组件列表，动态渲染表单

--- src/app/question/[id]/page.tsx
+++ src/app/question/[id]/page.tsx
@@ -1,24 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import QuestionInput from "@/components/QuestionComponents/QuestionInput";
-import QuestionRadio from "@/components/QuestionComponents/QuestionRadio";
 import styles from "./page.module.scss";
 import PageWrapper from "@/components/PageWrapper";
 import { getQuestionById } from "@/service/question";
-
-// type PropsType = {
-//     errno: number
-//     data?: {
-//         id: string,
-//         title: string,
-//         desc?: string,
-//         js?: string,
-//         css?: string,
-//         inPublished: boolean,
-//         isDeleted: boolean,
-//         componentList: Array<any>
-//     }
-//     msg?: string
-// }
+import { getComponent } from "@/components/QuestionComponents";
 
 export default async function QuestionPage({ params }: { params: { id: string } }) {
     const { id } = await params
@@ -44,24 +28,19 @@ export default async function QuestionPage({ params }: { params: { id: string } 
         </PageWrapper>
     }
 
+    // 遍历组件
+    const ComponentListElem = <>
+    {componentList.map(c => {
+        const ComponentElem = getComponent(c)
+        return <div className={styles.componentWrapper} key={c.fe_id}>{ComponentElem}</div>
+    })}
+    </>
+
     return (
         <PageWrapper title={title}>
             <form action="/api/answer" method="POST">
                 <input type="hidden" name="questionId" value={data?.id} />
-                <div className={styles.componentWrapper}>
-                    <QuestionInput fe_id="c1" props={{title: '你的姓名',placeholder: "请输入内容"}}/>
-                </div>
-                <div className={styles.componentWrapper}>
-                    <QuestionRadio fe_id="c2" props={{
-                        title: '你的性别',
-                        options: [
-                            {value: 'male', text: '男'},
-                            {value: 'female', text: '女'}
-                        ],
-                        value: 'male',
-                        isVertical: false
-                    }}/>
-                </div>
+                {ComponentListElem}
                 <div className={styles.submitBtnContainer}>
                     <button type="submit">提交</button>
                 </div>
